25df1cc3f23e20108045933173323869cb3c5a06
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-01-15 09:02:56 +0600

    User Profile Part 1

--- DjangoBlog/DjangoBlog/urls.py
+++ DjangoBlog/DjangoBlog/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
